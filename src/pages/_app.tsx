6cddd1fe0726def1576252851a4ea720dec63484
Kamil Anwar <kamilanwar@Kamils-MacBook-Pro.local>
2024-11-30 02:36:07 +0530

    Hope this not be it lol

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 
-import Header from '../components/header'
+import Header from '../components/header';
 import Footer from "../components/footer";
 import Body from "../components/body";
 import About from "../components/about";
