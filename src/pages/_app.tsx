7d26e40c1d50338aa263ac4f3440d539179c1802
al-habibi-1107 <kamilanwar2001@gmail.com>
2024-12-06 02:03:48 +0530

    Vercel build fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 
 
 import { ChakraProvider,defaultSystem } from "@chakra-ui/react"
-import { AppProps } from "next/app";
 
 import Header from '../components/header';
 import Footer from "../components/Footer";
@@ -25,7 +24,7 @@ import "../styles/brands.module.css";
 import "../styles/quickContact.module.css";
 
 
-export default function App({ Component, pageProps }:AppProps) {
+export default function App() {
   return (
     <ChakraProvider value={defaultSystem}>
       <Header />
